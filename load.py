adf1f66e5fd8d29d98595ed5dffbdebd894303db
fabio-scarel <114494196+fabio-scarel@users.noreply.github.com>
2024-08-01 17:29:11 -0300

    Update load.py

--- load.py
+++ load.py
@@ -1,38 +1,74 @@
 import pandas as pd
 import psycopg2
 import numpy as np
+import os
+from dotenv import load_dotenv
 
-proposicoes = pd.read_csv('proposicoes_clean.csv')
-tramitacoes = pd.read_csv('tramitacoes.csv')
+# Load environment variables from .env file
+load_dotenv()
 
-database_info = {
-    'DB_HOST': "localhost",
-    'DB_NAME': "khipo_db",
-    'DB_USER': "khipo_user",
-    'DB_PASSWORD': "khipo"
-}
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USER')
+db_pass = os.environ.get('DB_PASS')
+db_conn_uri = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
 
 
-def create_tables(database_info):
+def add_constraint(conn, table_name):
+    cursor = conn.cursor()
 
-    conn = psycopg2.connect(
-        host=database_info['DB_HOST'],
-        dbname=database_info['DB_NAME'],
-        user=database_info['DB_USER'],
-        password=database_info['DB_PASSWORD']
-    )
+    prop_query = f"""
+                        ALTER TABLE proposicao
+                        ADD CONSTRAINT unique_proposicao_city 
+                        UNIQUE (author, presentationdate, ementa, regime, situation, propositiontype, number, year, city, state)
+                    """
+    
+    tram_query = f"""
+                        ALTER TABLE tramitacao
+                        ADD CONSTRAINT unique_tramitacao 
+                        UNIQUE (createdat, description, local, propositionid)
+                    """
 
-    cur = conn.cursor()
+    if table_name == 'proposicao':    
+        cursor.execute(prop_query)
+    else:
+        cursor.execute(tram_query)
+
+    conn.commit()
+    cursor.close()
+
+
+def correct_typing(proposicoes, tramitacoes, load=True):
+
+    if load:
+        proposicoes['presentationdate'] = proposicoes['presentationdate'].apply(
+            lambda date: pd.to_datetime(date) if date is not None else None)
+        tramitacoes['createdat'] = tramitacoes['createdat'].apply(
+            lambda date: pd.to_datetime(date) if date is not None else None)
+
+        proposicoes['presentationdate'] = proposicoes['presentationdate'].replace({pd.NaT: None, np.nan: None})
+        proposicoes['year'] = proposicoes['year'].replace({pd.NaT: None, np.nan: None})
+
+        return proposicoes, tramitacoes
+    
+    else:
+        proposicoes['year'] = proposicoes['year'].replace({pd.NaT: None, np.nan: None})
+
+        return proposicoes, tramitacoes
+
+
+def create_tables(db_conn_uri):
 
     create_table_proposicao = """
     CREATE TABLE IF NOT EXISTS Proposicao (
-        id INTEGER PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         author TEXT,
-        presentationDate TIMESTAMP,
+        presentationdate TIMESTAMP,
         ementa TEXT,
         regime TEXT,
         situation TEXT,
-        propositionType TEXT,
+        propositiontype TEXT,
         number TEXT,
         year INTEGER,
         city VARCHAR(500),
@@ -41,81 +77,87 @@ def create_tables(database_info):
     """
 
     create_table_tramitacao = """
-    CREATE TABLE IF NOT EXISTS Tramitacao (
-        id INTEGER PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS tramitacao (
+        id SERIAL PRIMARY KEY,
         createdAt TIMESTAMP,
         description TEXT,
         local TEXT,
-        propositionId INTEGER REFERENCES Proposicao(id)
+        propositionid INTEGER REFERENCES proposicao(id)
     );
     """
 
-    cur.execute(create_table_proposicao)
-    cur.execute(create_table_tramitacao)
-    conn.commit()
+    with psycopg2.connect(db_conn_uri) as conn:
+        cur = conn.cursor()
 
-    cur.close()
-    conn.close()
+        cur.execute(create_table_proposicao)
+        cur.execute(create_table_tramitacao)
+        conn.commit()
 
-
-def insert_data(proposicoes, tramitacoes, database_info):
-    proposicoes['presentationDate'] = proposicoes['presentationDate'].apply(
-        lambda date: pd.to_datetime(date) if date is not None else None)
-    tramitacoes['createdAt'] = tramitacoes['createdAt'].apply(
-        lambda date: pd.to_datetime(date) if date is not None else None)
-
-    proposicoes['presentationDate'] = proposicoes['presentationDate'].replace({pd.NaT: None, np.nan: None})
-    proposicoes['year'] = proposicoes['year'].replace({pd.NaT: None, np.nan: None})
-
-    conn = psycopg2.connect(
-        host=database_info['DB_HOST'],
-        dbname=database_info['DB_NAME'],
-        user=database_info['DB_USER'],
-        password=database_info['DB_PASSWORD']
-    )
-
-    cur = conn.cursor()
-
-    for index, row in proposicoes.iterrows():
-        cur.execute("""
-        INSERT INTO Proposicao (id, author, presentationDate, ementa, regime, situation, propositionType, number, year, city, state)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        ON CONFLICT (id) DO UPDATE SET
-            author = EXCLUDED.author,
-            presentationDate = EXCLUDED.presentationDate,
-            ementa = EXCLUDED.ementa,
-            regime = EXCLUDED.regime,
-            situation = EXCLUDED.situation,
-            propositionType = EXCLUDED.propositionType,
-            number = EXCLUDED.number,
-            year = EXCLUDED.year,
-            city = EXCLUDED.city,
-            state = EXCLUDED.state
-        """, (row['id'], row['author'], row['presentationDate'], row['ementa'], row['regime'],
-              row['situation'], row['propositionType'], row['number'], row['year'],
-              row['city'], row['state']))
-
-    conn.commit()
-
-    for index, row in tramitacoes.iterrows():
-        cur.execute("""
-        INSERT INTO Tramitacao (id, createdAt, description, local, propositionId)
-        VALUES (%s, %s, %s, %s, %s)
-        ON CONFLICT (id) DO UPDATE SET
-            createdAt = EXCLUDED.createdAt,
-            description = EXCLUDED.description,
-            local = EXCLUDED.local,
-            propositionId = EXCLUDED.propositionId
-        """, (row['id'], row['createdAt'], row['description'], row['local'], row['propositionId']))
-
-    conn.commit()
-    cur.close()
-    conn.close()
+        cur.close()
 
 
-def load_main(proposicoes, tramitacoes, database_info):
-    create_tables(database_info)
-    insert_data(proposicoes, tramitacoes, database_info)
+def insert_data(proposicoes, tramitacoes, db_conn_uri):
+
+    with psycopg2.connect(db_conn_uri) as conn:
+
+        cur = conn.cursor()
+
+        for index, row in proposicoes.iterrows():
+            cur.execute("""
+            INSERT INTO Proposicao (id, author, presentationdate, ementa, regime, situation, propositiontype, number, year, city, state)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (id) DO UPDATE SET
+                author = EXCLUDED.author,
+                presentationdate = EXCLUDED.presentationdate,
+                ementa = EXCLUDED.ementa,
+                regime = EXCLUDED.regime,
+                situation = EXCLUDED.situation,
+                propositiontype = EXCLUDED.propositiontype,
+                number = EXCLUDED.number,
+                year = EXCLUDED.year,
+                city = EXCLUDED.city,
+                state = EXCLUDED.state
+            """, (row['id'], row['author'], row['presentationdate'], row['ementa'], row['regime'],
+                row['situation'], row['propositiontype'], row['number'], row['year'],
+                row['city'], row['state']))
+
+        conn.commit()
+
+        for index, row in tramitacoes.iterrows():
+            cur.execute("""
+            INSERT INTO Tramitacao (id, createdat, description, local, propositionid)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (id) DO UPDATE SET
+                createdat = EXCLUDED.createdat,
+                description = EXCLUDED.description,
+                local = EXCLUDED.local,
+                propositionid = EXCLUDED.propositionid
+            """, (row['id'], row['createdat'], row['description'], row['local'], row['propositionid']))
+
+        
+        conn.commit()
+        cur.close()
+
+
+def load_main(proposicoes, tramitacoes, db_conn_uri):
+
+    proposicoes, tramitacoes = correct_typing(proposicoes, tramitacoes)
+    create_tables(db_conn_uri)
+    insert_data(proposicoes, tramitacoes, db_conn_uri)
+
+    with psycopg2.connect(db_conn_uri) as conn:
+        add_constraint(conn, 'proposicao')
+        add_constraint(conn, 'tramitacao')
+
+    return 'Dados carregados'
+
+
+if __name__ == '__main__':
+
+    proposicoes = pd.read_csv('proposicoes_clean.csv')
+    tramitacoes = pd.read_csv('tramitacoes.csv')
+    
+    print(load_main(proposicoes, tramitacoes, db_conn_uri))
 
     return 'Dados carregados'
 
